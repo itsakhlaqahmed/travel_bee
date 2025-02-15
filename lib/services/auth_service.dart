130a9cb54e8769166da8bdef71b3494a8a68a7f1
Akhlaq Ahmed <akhlaqahmed990@gmail.com>
2025-02-16 03:36:52 +0500

    Add auth_service.dart

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  AuthService();
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  User? get currentUser => _firebaseAuth.currentUser;
+
+  Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
+
+  User? get getAuth => _firebaseAuth.currentUser; // retruns current user if auth else null;
+
+  Future<User?> signInWithEmailAndPassword(
+      {required String email, required String password}) async {
+    final UserCredential userCredential = await _firebaseAuth
+        .signInWithEmailAndPassword(email: email, password: password);
+
+    return userCredential.user;
+  }
+
+  Future<User?> createUserWithEmailAndPassword(
+      {required String email, required String password}) async {
+    final userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    return userCredential.user;
+  }
+
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+}
